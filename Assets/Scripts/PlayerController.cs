8e66e4ae2c4dbd6c3004d003fc9f0d3fe07b4512
Darryl Tolentino <darryl.tolentino@gmail.com>
2019-06-04 23:56:25 -0700

    Adding PlayerController script for movement and constricting Player to freeze rotation on movement.
    Adding PlayerController script for movement and constricting Player to freeze rotation on movement. We want player to constantly be upright while moving across the plane.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    private Vector3 moveInput;
+    private Rigidbody rb;
+    private Vector3 moveVelocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveInput = new Vector3 (Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput * moveSpeed;
+    }
+
+    void FixedUpdate() {
+        rb.velocity = moveVelocity;
+    }
+}
