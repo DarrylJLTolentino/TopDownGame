c9dd773480067483221f3c7529bd18bf919e5249
Darryl Tolentino <darryl.tolentino@gmail.com>
2019-06-05 00:13:57 -0700

    Adding Raycast for player in PlayerController.cs
    Adding Raycast for player to look in direction of mouse position, but still looking across z-axis within PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,17 +8,34 @@ public class PlayerController : MonoBehaviour
     private Vector3 moveInput;
     private Rigidbody rb;
     private Vector3 moveVelocity;
+    private Camera mainCamera;
     // Start is called before the first frame update
     void Start()
     {
+        //Need to get Rigidbody of player and find the main camera within Unity
         rb = GetComponent<Rigidbody>();
+        mainCamera = FindObjectOfType<Camera>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Since game is on 3D plane, movement input must be a 3D vector (x, y, z)
         moveInput = new Vector3 (Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
         moveVelocity = moveInput * moveSpeed;
+
+        //Ray is used to create a line from the camera to position of mouse
+        Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayLength;
+
+        if(groundPlane.Raycast(cameraRay, out rayLength)) 
+        {
+            Vector3 pointToLook = cameraRay.GetPoint(rayLength);
+            Debug.DrawLine(cameraRay.origin, pointToLook, Color.blue);
+
+            transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
+        }
     }
 
     void FixedUpdate() {
